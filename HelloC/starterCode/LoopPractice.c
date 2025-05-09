ef683ca4e62bce20527738f9c1a1677208a79f09
blouyakis <bobbilouyakis@icloud.com>
2025-05-08 21:45:52 -0400

    whileloop edits

--- HelloC/starterCode/LoopPractice.c
+++ HelloC/starterCode/LoopPractice.c
@@ -27,8 +27,59 @@ void forLoopPractice(int start, int end) {
     }
 }
 
+void whileLoopPractice(int start, int end) {
+    // while loop
+    int i = start;
+    while(i < end) {
+        printf("While Loop: %d\n", i);
+        i++;
+    }
+
+    i = end - 1;
+    while (i >= start) {
+        printf("While Loop Reversed: %d\n", i);
+        i--;
+    }
+
+    // Now with a step
+    i = start;
+    while (i < end) {
+        printf("While Loop with Step: %d\n", i);
+        i += 2;
+    }
+}
+
+void whileLoopMenu() {
+    int choice = 0;
+    while (choice != 3) {
+        printf("1. For Loop\n");
+        printf("2. While Loop\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                forLoopPractice(0, 10);
+                break;
+            case 2:
+                whileLoopPractice(0, 10);
+                break;
+            case 3:
+                printf("Exiting...");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
+        }
+
+    }
+}
 
 int main() {
-    forLoopPractice(0, 10);
+    whileLoopMenu();
+    // forLoopPractice(0, 10);
+    // whileLoopPractice(0, 10);
     return 0;
 }
