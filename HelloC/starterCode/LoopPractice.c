4ea666c62d00affb3ca2fc01fb62de110e6d80f1
blouyakis <bobbilouyakis@icloud.com>
2025-05-08 21:20:48 -0400

    looppractice edits

--- HelloC/starterCode/LoopPractice.c
+++ HelloC/starterCode/LoopPractice.c
@@ -11,10 +11,24 @@
 #include <stdlib.h>
 
 
+void forLoopPractice(int start, int end) {
+    // for Loop
+    for (int i = start; i < end; i++) {
+        printf("For Loop %d\n", i);
+    }
 
+    // reverse Loop
+    for (int i = end - 1; i >= start; i--) {
+        printf("For Loop Reverse %d\n", i);
+    }
+
+    for (int i = start; i < end; i+=2) {
+        printf("For Loop with step: %d\n", i);
+    }
+}
 
 
 int main() {
-    
+    forLoopPractice(0, 10);
     return 0;
 }
