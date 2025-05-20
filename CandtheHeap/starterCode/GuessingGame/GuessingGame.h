c1103f6ab31390dda84285131df7a3bc5b365f89
blouyakis <bobbilouyakis@icloud.com>
2025-05-20 15:50:55 -0400

    guessing game update

--- CandtheHeap/starterCode/GuessingGame/GuessingGame.h
+++ CandtheHeap/starterCode/GuessingGame/GuessingGame.h
@@ -1,6 +1,34 @@
 #ifndef GUESSINGGAME_H
 #define GUESSINGGAME_H
 
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define DEFAULT_MAX_ATTEMPTS 10
+#define DEFAULT_MIN 1
+#define DEFAULT_MAX 100
+#define HUMAN 1
+#define COMPUTER 0
+
+typedef struct {
+    int *guesses;
+    int attempts;
+    int maxAttempts;
+    int playerType;
+    int min;
+    int max;
+} GameState;
+
+GameState *initializeGameState(int maxAttempts, int playerType, int min, int max);
+void freeGameState(GameState *gameState);
+
+int getRandomNumber(int min, int max);
+int getInput(void);
+bool runGame(int randomNumber, GameState *gameState);
+void giveFeedback(int randomNumber, bool found, GameState *gameState);
+void start(void);
+
+void feedback(int state); // prints too high or too low
 
 
 #endif // GUESSINGAME_H
--- CandtheHeap/starterCode/GuessingGame/TestDriver.c
+++ CandtheHeap/starterCode/GuessingGame/TestDriver.c
@@ -7,10 +7,19 @@
 #include <stdlib.h>
 #include <time.h>
 
+void testGetInput() {
+  printf("Enter a value: ");
+  int myValue = getInput();
+  printf("Testing my value by entering 10, the result is %d", myValue);
+}
+
 int main() {
   srand(0); // Seed the random number generator with a fixed value for
             // reproducibility
   int randomNumber = getRandomNumber(DEFAULT_MIN, DEFAULT_MAX);
   printf("Random number generated: %d\n", randomNumber);
+
+  testGetInput();
   return 0;
 }
+
